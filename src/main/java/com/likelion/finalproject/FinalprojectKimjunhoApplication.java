4bffe307c404c2afd27fc8326100efa8972ef2cd
chordpli <chordplaylist@gmail.com>
2022-12-20 10:41:42 +0900

    build: build project

---
+++ src/main/java/com/likelion/finalproject/FinalprojectKimjunhoApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectKimjunhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectKimjunhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/finalproject/FinalprojectKimjunhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectKimjunhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
