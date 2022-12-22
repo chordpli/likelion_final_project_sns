d68cfb773ccdedcc2d0f1d109d1acf1573936255
chordpli <chordplaylist@gmail.com>
2022-12-23 01:43:30 +0900

    feat(ExceptionManager): sqlExceptionHandler 추가
    1. sql관련 예외가 발생할 경우 핸들링

--- src/main/java/com/likelion/finalproject/exception/ExceptionManager.java
+++ src/main/java/com/likelion/finalproject/exception/ExceptionManager.java
@@ -1,10 +1,12 @@
 package com.likelion.finalproject.exception;
 
 import com.likelion.finalproject.domain.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +19,15 @@ public class ExceptionManager {
         result.put("errorCode", e.getErrorCode());
         result.put("message", e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error("ERROR", result));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlExceptionHandler(SQLException e) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("errorCode", e.getErrorCode());
+        result.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error("ERROR", result));
     }
 }
