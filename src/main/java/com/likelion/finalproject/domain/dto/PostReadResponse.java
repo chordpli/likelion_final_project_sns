503bda975e5afab950081cee7f2ac3af987f842d
chordpli <chordplaylist@gmail.com>
2022-12-23 22:31:27 +0900

    feat(Post): LocalDateTime @JsonFormat 생성
    1. DB에 저장되어 있는 시간은 소수점까지 저장되므로 반환될 때 원하는 패턴으로 변경하여 반환

--- src/main/java/com/likelion/finalproject/domain/dto/PostReadResponse.java
+++ src/main/java/com/likelion/finalproject/domain/dto/PostReadResponse.java
@@ -1,5 +1,6 @@
 package com.likelion.finalproject.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,8 @@ public class PostReadResponse {
     private String title;
     private String body;
     private String userName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime lastModifiedAt;
 }
