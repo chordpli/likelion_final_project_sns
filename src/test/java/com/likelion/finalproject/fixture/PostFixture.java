092d14967ddcad1a0c68fad5039744cef7aec176
chordpli <chordplaylist@gmail.com>
2022-12-27 01:09:42 +0900

    feat: get 메서드 오버로딩

--- src/test/java/com/likelion/finalproject/fixture/PostFixture.java
+++ src/test/java/com/likelion/finalproject/fixture/PostFixture.java
@@ -1,24 +1,25 @@
 package com.likelion.finalproject.fixture;
 
-import com.likelion.finalproject.domain.dto.PostDto;
-import com.likelion.finalproject.domain.dto.PostReadResponse;
 import com.likelion.finalproject.domain.entity.Post;
 import com.likelion.finalproject.domain.entity.User;
-import com.likelion.finalproject.domain.enums.UserRole;
-
-import java.time.LocalDateTime;
 
 public class PostFixture {
     public static Post get() {
         User user = UserFixture.get("chordpli", "1234");
-
         return Post.builder()
                 .id(1)
                 .user(user)
                 .title("title")
                 .body("body")
                 .build();
+    }
 
-
+    public static Post get(User user) {
+        return Post.builder()
+                .id(1)
+                .user(user)
+                .title("title")
+                .body("body")
+                .build();
     }
 }
