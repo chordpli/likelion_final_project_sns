0cbd7123457200f133e1dd27b743eaede7c99aea
chordpli <chordplaylist@gmail.com>
2022-12-27 01:10:03 +0900

    test: fixture 사용하여 리팩토링

--- src/test/java/com/likelion/finalproject/service/PostServiceTest.java
+++ src/test/java/com/likelion/finalproject/service/PostServiceTest.java
@@ -41,8 +41,7 @@ class PostServiceTest {
     @DisplayName("조회 성공")
     void success_get_post() {
         Post fixture = PostFixture.get();
-
-        when(postRepository.findById(fixture.getId())).thenReturn(Optional.of(fixture));
+        when(postRepository.findById(any())).thenReturn(Optional.of(fixture));
         PostReadResponse response = postService.getPost(fixture.getId());
         assertEquals(fixture.getUser().getUserName(), response.getUserName());
     }
@@ -51,37 +50,28 @@ class PostServiceTest {
     @Test
     @DisplayName("등록 성공")
     void success_post() {
-        Post fixture = PostFixture.get();
+        User user = UserFixture.get("chordpli", "1234");
+        PostRequest request = new PostRequest("title", "body");
 
-        PostReadResponse response = PostReadResponse.builder()
-                .id(fixture.getId())
-                .title(fixture.getTitle())
-                .body(fixture.getBody())
-                .userName(fixture.getUser().getUserName())
-                .createdAt(LocalDateTime.now())
-                .lastModifiedAt(LocalDateTime.now())
-                .build();
-
-        User mockUser = mock(User.class);
         Post mockPost = mock(Post.class);
 
-        when(userRepository.findByUserName(response.getUserName()))
-                .thenReturn(Optional.of(mockUser));
+        when(userRepository.findByUserName(any()))
+                .thenReturn(Optional.of(user));
         when(postRepository.save(any()))
                 .thenReturn(mockPost);
 
-        Assertions.assertDoesNotThrow(() -> postService.post(new PostRequest(response.getTitle(), response.getBody()), response.getUserName()));
+        Assertions.assertDoesNotThrow(() -> postService.post(request, user.getUserName()));
     }
 
     @Test
     @DisplayName("등록 실패_존재하지 않은 유저")
     void fail_post_not_exist_user() {
-        Post fixture = PostFixture.get();
-        when(userRepository.findByUserName(fixture.getUser().getUserName())).thenReturn(Optional.empty());
-        when(postRepository.save(any())).thenReturn(mock(Post.class));
+        User user = UserFixture.get("chordpli", "1234");
+        PostRequest request = new PostRequest("title", "body");
+        when(userRepository.findByUserName(user.getUserName())).thenReturn(Optional.empty());
         SNSAppException exception
                 = Assertions.assertThrows(SNSAppException.class,
-                () -> postService.post(new PostRequest(fixture.getTitle(), fixture.getBody()), fixture.getUser().getUserName()));
+                () -> postService.post(request, user.getUserName()));
         Assertions.assertEquals(ErrorCode.USERNAME_NOT_FOUND, exception.getErrorCode());
     }
 
@@ -90,7 +80,7 @@ class PostServiceTest {
     @DisplayName("수정 실패_포스트가 존재하지 않음")
     void fail_modify_post_not_exist_post() {
         Post fixture = PostFixture.get();
-        when(userRepository.findByUserName(fixture.getUser().getUserName())).thenReturn(Optional.of(mock(User.class)));
+        when(userRepository.findByUserName(any())).thenReturn(Optional.of(mock(User.class)));
         when(postRepository.save(any())).thenReturn(Optional.empty());
         SNSAppException exception
                 = Assertions.assertThrows(SNSAppException.class,
@@ -118,13 +108,26 @@ class PostServiceTest {
     }
 
     @Test
+    @DisplayName("수정 실패_유저가 존재하지 않음")
+    void fail_modify_post_not_exist_user() {
+        Post fixture = PostFixture.get();
+
+        when(userRepository.findByUserName(fixture.getUser().getUserName())).thenReturn(Optional.empty());
+
+        SNSAppException exception
+                = Assertions.assertThrows(SNSAppException.class,
+                () -> postService.modifyPost(fixture.getId(), new PostModifyRequest(fixture.getTitle(), fixture.getBody()), fixture.getUser().getUserName()));
+        Assertions.assertEquals(ErrorCode.USERNAME_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
     @DisplayName("수정 성공")
-    void success_modify_post(){
+    void success_modify_post() {
         Post fixture = PostFixture.get();
         User user = UserFixture.get("chordpli", "1234");
 
-        when(postRepository.findById(fixture.getId())).thenReturn(Optional.of(fixture));
-        when(userRepository.findByUserName(fixture.getUser().getUserName())).thenReturn(Optional.of(user));
+        when(postRepository.findById(any())).thenReturn(Optional.of(fixture));
+        when(userRepository.findByUserName(any())).thenReturn(Optional.of(user));
 
         PostModifyRequest request = new PostModifyRequest("수정 제목", "수정 내용");
 
@@ -168,7 +171,7 @@ class PostServiceTest {
 
     @Test
     @DisplayName("삭제 성공")
-    void success_modify_delete(){
+    void success_modify_delete() {
         Post fixture = PostFixture.get();
         User user = UserFixture.get("chordpli", "1234");
 
