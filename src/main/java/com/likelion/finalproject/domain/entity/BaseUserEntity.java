dbbba386948a03fc99df30b7d10e5341c0a1de20
chordpli <chordplaylist@gmail.com>
2022-12-28 22:08:43 +0900

    refactor: 게시물과 변수명이 다르기 때문에 baseentity 추가 생성

---
+++ src/main/java/com/likelion/finalproject/domain/entity/BaseUserEntity.java
@@ -0,0 +1,30 @@
+package com.likelion.finalproject.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public abstract class BaseUserEntity {
+    /* 사용할 일이 없으므로 설계상 추상클래스로 만드는 것이 실수를 방지할 수 있다 */
+
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime registeredAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
