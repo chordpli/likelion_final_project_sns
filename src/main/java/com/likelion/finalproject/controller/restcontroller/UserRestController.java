9b84b7aa9de5546cac0dc6903dbfd20609adfbe5
chordpli <chordplaylist@gmail.com>
2023-01-03 10:17:19 +0900

    refactor: @ApiOperation 추가

--- src/main/java/com/likelion/finalproject/controller/restcontroller/UserRestController.java
+++ src/main/java/com/likelion/finalproject/controller/restcontroller/UserRestController.java
@@ -3,6 +3,7 @@ package com.likelion.finalproject.controller.restcontroller;
 import com.likelion.finalproject.domain.Response;
 import com.likelion.finalproject.domain.dto.*;
 import com.likelion.finalproject.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -16,12 +17,14 @@ public class UserRestController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest dto){
         UserJoinResponse user = userService.join(dto);
         return Response.success(user);
     }
 
+    @ApiOperation(value = "회원 등급 변경")
     @PostMapping("/{userId}/role/change")
     public Response<UserSwithResponse> changeUserRoleToAdmin(@PathVariable Integer userId, Authentication authentication){
         log.info("toAdmin userId ={}", userId);
@@ -29,6 +32,7 @@ public class UserRestController {
         return Response.success(user);
     }
 
+    @ApiOperation(value = "로그인")
     @PostMapping("/login")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest dto){
         UserLoginResponse loginUser = userService.login(dto);
