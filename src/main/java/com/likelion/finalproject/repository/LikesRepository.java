b25bd7461c91cd5448e4f9517c10a2a08e22d7b7
chordpli <chordplaylist@gmail.com>
2023-01-09 17:09:27 +0900

    test: Service Test를 위한 repository method

--- src/main/java/com/likelion/finalproject/repository/LikesRepository.java
+++ src/main/java/com/likelion/finalproject/repository/LikesRepository.java
@@ -19,4 +19,8 @@ public interface LikesRepository extends JpaRepository<Likes, Integer> {
     void deleteAllByPost(Post post);
 
     List<Likes> findAllByPost(Post post);
+
+    Optional<Likes> findByPostId(Integer postId);
+
+    Optional<Likes> findByPostIdAndUserId(Integer postId, Integer userId);
 }
